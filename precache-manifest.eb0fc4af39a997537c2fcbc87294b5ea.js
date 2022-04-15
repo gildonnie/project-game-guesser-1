38bee382549550a4a4e00ad4e2453f8001136909
donnie gil <gildonnie864@gmail.com>
2022-04-15 08:07:04 -0700

    Updates

---
+++ precache-manifest.eb0fc4af39a997537c2fcbc87294b5ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6602956b773df802c0d9",
+    "url": "/project-game-guesser-1/static/css/main.708a12dd.chunk.css"
+  },
+  {
+    "revision": "6602956b773df802c0d9",
+    "url": "/project-game-guesser-1/static/js/main.6602956b.chunk.js"
+  },
+  {
+    "revision": "99ef2e7be417a9336e82",
+    "url": "/project-game-guesser-1/static/js/1.99ef2e7b.chunk.js"
+  },
+  {
+    "revision": "a5c71b18ea90961d39af",
+    "url": "/project-game-guesser-1/static/js/runtime~main.a5c71b18.js"
+  },
+  {
+    "revision": "a134fd379f4d04e220c6fef545986d97",
+    "url": "/project-game-guesser-1/static/media/fastheartbeat.a134fd37.mp3"
+  },
+  {
+    "revision": "dc2120cc273eac5de965829410182601",
+    "url": "/project-game-guesser-1/static/media/flatline.dc2120cc.mp3"
+  },
+  {
+    "revision": "7135cda1421576107083e6eba66e9f78",
+    "url": "/project-game-guesser-1/static/media/scarylaugh2.7135cda1.mp3"
+  },
+  {
+    "revision": "0a0a5aca407944fce039d9fffc52abe0",
+    "url": "/project-game-guesser-1/index.html"
+  }
+];
